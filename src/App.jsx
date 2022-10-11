fe2051eaae538bc278a9824ef39a23c9c2ce04c7
Dougie <douglaswesson@hotmail.co.uk>
2022-10-11 10:34:21 +0100

    init project start conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
 import "./App.css";
-import Person from "./Person";
 
 function App() {
-  const people = [
-    {
-      name: "Dougie",
-      age: "26",
-      hobby: "Archery",
-      height: "2 dogs",
-    },
-    {
-      name: "Ben",
-      age: "29",
-      hobby: "Being old",
-    },
-    {
-      name: "Dave",
-      age: "Lots",
-      hobby: "Gaming",
-    },
-    {
-      name: "Shelly",
-      age: "Lots+",
-      hobby: "Gaming",
-    },
-  ];
-
   return (
-    <>
-      {people.map((person) => (
-        <Person person={person}/>
-      ))}
-    </>
-  );
+    null
+  )
 }
 
 export default App;
